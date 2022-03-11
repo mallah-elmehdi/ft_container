041e703dee988936259b8a712ea9aa780cf22f11
mallah-elmehdi <elmehdimallah@gmail.com>
2022-03-11 23:35:13 +0100

    vector

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -1,40 +1,48 @@
 #ifndef _VECTOR_HPP_
 #define _VECTOR_HPP_
-// ============ FT NAMESPACE 
+// ============ FT NAMESPACE
 namespace ft {
-	/* include libraries */ 
+	/* include libraries */
 	# include <iostream>
 	# include <memory>
 
 	/* vector class template */
-	template <class T, class alloc = std::allocator<T> >
+	template <class T, class Alloc = std::allocator<T> >
 	class vector {
 		private:
 			alloc _alloc;
 			T *array;
 		public:
-			typedef T value_type;
-			//default (1)
+			/* member type */
+			typedef T									value_type;
+			typedef Alloc								allocator_type;
+			typedef allocator_type::reference			reference;
+			typedef allocator_type::const_reference		const_reference;
+			typedef allocator_type::pointer				pointer;
+			typedef allocator_type::const_pointer		const_pointer;
+			typedef allocator_type::reference			const_reference;
+			typedef allocator_type::reference			const_reference;
+
+			/* default */
 			explicit vector (const allocator_type& alloc = allocator_type());
-			//fill (2)	
-			explicit vector (size_type n, const value_type& val = value_type(),
-							const allocator_type& alloc = allocator_type());
-			//range (3)	
+			
+			/* fill */
+			explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type());
+			
+			/* range */
 			//template <class InputIterator>
 			//		vector (InputIterator first, InputIterator last,
 			//				const allocator_type& alloc = allocator_type());
-			//copy (4)	
-			vector (const vector& x);	
+			
+			/* copy */
+			vector (const vector& x);
 
-			vector() 
-			{
-				/* constructor */
-			}
 			/* one parametre */
 			vector(size_t lenght)
 			{
 				this->array = _alloc.allocate(lenght);
 			}
+			
 			/* two parametres */
 			vector(size_t lenght, T init_value)
 			{
@@ -43,12 +51,9 @@ namespace ft {
 				{
 					this->array[i] = init_value;
 				}
-				
+
 			}
-			~vector()
-			{
-				/* dectructor */
-			}
+
 			/* [] overwrite */
 			T get(size_t index)
 			{
