5dd5ea7471d76538c7d4fcb143589f5de62a0921
mallah-elmehdi <elmehdimallah@gmail.com>
2022-03-11 22:22:40 +0100

    let's do it

---
+++ push.sh
@@ -0,0 +1,21 @@
+
+   
+#!/bin/bash
+
+Color_Off='\033[0m'       # Text Reset
+BPurple='\033[1;35m'      # Background Purple
+On_Green='\033[42m'       # Green
+On_Yellow='\033[43m'      # Yellow
+
+#while true
+#do
+        #sleep 300
+		#echo
+        #echo "${On_Green}                  PUSHING                 ${Color_Off}"
+        #git add .
+		#echo -n "${BPurple}please enter the commit message: ${Color_Off}"
+		#read commitmessage
+        #git commit -m "$commitmessage"
+        #git push
+        #echo "${On_Yellow} FINISHED : [THE NEXT PUSH IN 5 MINUTES] ${Color_Off}"
+#done
---
+++ vector/vector.hpp
@@ -0,0 +1,45 @@
+#ifndef _VECTOR_HPP_
+#define _VECTOR_HPP_
+// ============ FT NAMESPACE 
+namespace ft {
+	/* include libraries */ 
+	# include <iostream>
+	# include <memory>
+
+	/* vector class template */
+	template <class T, class alloc = std::allocator<T> >
+	class vector {
+		private:
+			alloc _alloc;
+		protected:
+			T *array;
+		public:
+			vector() {}
+			/* one parametre */
+			vector(size_t lenght)
+			{
+				this->array = _alloc.allocate(lenght);
+			}
+			/* two parametres */
+			vector(size_t lenght, T init_value)
+			{
+				this->array = _alloc.allocate(lenght);
+				for (size_t i = 0; i < lenght; i++)
+				{
+					this->array[i] = init_value;
+				}
+				
+			}
+			~vector()
+			{
+
+			}
+			/* [] overwrite */
+			T get(size_t index)
+			{
+				return array[index];
+			}
+	};
+
+}
+#endif
