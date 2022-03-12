773d4f1cee37d2609291a2701dbc0a0c54d96d53
mallah-elmehdi <elmehdimallah@gmail.com>
2022-03-12 17:47:44 +0100

    vector

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -1,11 +1,10 @@
 #ifndef _VECTOR_HPP_
 #define _VECTOR_HPP_
-// ============ FT NAMESPACE
+// ****************** FT NAMESPACE
 namespace ft {
 	/* include libraries */
 	# include <iostream>
 	# include <memory>
-
 	/* vector class template */
 	template <class T, class Alloc = std::allocator<T> >
 	class vector {
@@ -20,31 +19,26 @@ namespace ft {
 			typedef typename allocator_type::pointer			pointer;
 			typedef typename allocator_type::const_pointer		const_pointer;
 			typedef size_t										size_type;
-
 			/* default */
 			explicit vector (const allocator_type& alloc = allocator_type())
 			{
 				this->array = alloc.allocate();
 			}
-			
 			/* fill */
 			explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
 			{
-				alloc.allocate(n);
+				this->array = alloc.allocate(n);
 				for (size_type i = 0; i < n; i++)
 				{
 					this->array[i] = val;
 				}
 			}
-			
 			/* range */
 			//template <class InputIterator>
 			//		vector (InputIterator first, InputIterator last,
 			//				const allocator_type& alloc = allocator_type());
-			
 			/* copy */
 			//vector (const vector& x);
-
 			/* [] overwrite */
 			value_type get(size_t index)
 			{
