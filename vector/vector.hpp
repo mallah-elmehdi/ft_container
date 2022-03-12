e5c9afd9b7db6caa8ca203e216248647ffb5095e
mallah-elmehdi <elmehdimallah@gmail.com>
2022-03-12 15:42:20 +0100

    vector

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -10,16 +10,16 @@ namespace ft {
 	template <class T, class Alloc = std::allocator<T> >
 	class vector {
 		private:
-			pointer array;
+			T *array;
 		public:
 			/* member type */
-			typedef T				value_type;
-			typedef Alloc			allocator_type;
-			typedef T&				reference;
-			typedef const T&		const_reference;
-			typedef T*				pointer;
-			typedef const T*		const_pointer;
-			typedef size_t			size_type;
+			typedef T											value_type;
+			typedef Alloc										allocator_type;
+			typedef typename allocator_type::reference			reference;
+			typedef typename allocator_type::const_reference	const_reference;
+			typedef typename allocator_type::pointer			pointer;
+			typedef typename allocator_type::const_pointer		const_pointer;
+			typedef size_t										size_type;
 
 			/* default */
 			explicit vector (const allocator_type& alloc = allocator_type())
