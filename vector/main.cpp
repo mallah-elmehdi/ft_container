ad53a537931f4024f552035315b2f3b35998c6f0
mallah-elmehdi <elmehdimallah@gmail.com>
2022-03-14 22:36:49 +0100

    vector

--- vector/main.cpp
+++ vector/main.cpp
@@ -5,10 +5,11 @@
 int main()
 {
 	std::vector<int> test(5);
-	std::iterator<int>
+	std::vector<int>::iterator it = test.begin();
 	for (size_t i = 0; i < 5; i++)
 	{
 		test[i] = i;
 	}
-	std::cout << test << std::endl;
+	it + 1;
+	std::cout << *it << std::endl;
 }
