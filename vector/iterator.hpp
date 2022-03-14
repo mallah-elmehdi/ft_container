479df2fd9b6f15088964c5f5ca557664623a9cc6
mallah-elmehdi <elmehdimallah@gmail.com>
2022-03-14 17:21:13 +0100

    vector

--- vector/iterator.hpp
+++ vector/iterator.hpp
@@ -5,9 +5,8 @@ namespace ft {
 	/* include libraries */
 	# include <iostream>
 	# include <memory>
-	std::vector<int>;
 	/* vector class template */
-	template <class T, class Alloc = std::allocator<T> >
+	template <class T>
 	class vector {
 		public:
 			/* member type */
@@ -17,7 +16,7 @@ namespace ft {
 			typedef typename allocator_type::const_reference			const_reference;
 			typedef typename allocator_type::pointer					pointer;
 			typedef typename allocator_type::const_pointer				const_pointer;
-			typedef typename ft::iterator<T> 							iterator;
+			//typedef typename ft::iterator<T> 							iterator;
 			typedef size_t												size_type;
 		private:
 			pointer array;
