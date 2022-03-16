6293626e57a1fcb9aba2a920beb723e675b1e9c7
mallah-elmehdi <elmehdimallah@gmail.com>
2022-03-16 15:43:08 +0100

    working on vector

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -2,27 +2,29 @@
 #define _VECTOR_HPP_
 // * FT NAMESPACE - [VECTOR] *  
 namespace ft {
-	/* include libraries */
+	// + + + + + + + + + + + + + include libraries
 	# include <iostream>
 	# include <memory>
-	/* vector class template */
-	template <class T, class Alloc = std::allocator<T> >
+	// + + + + + + + + + + + + + vector class template
+	template <class T, class Allocator = std::allocator<T> >
 	class vector {
 		public:
-			/* member type */
-			typedef T													value_type;
-			typedef Alloc												allocator_type;
-			typedef typename allocator_type::reference					reference;
-			typedef typename allocator_type::const_reference			const_reference;
-			typedef typename allocator_type::pointer					pointer;
-			typedef typename allocator_type::const_pointer				const_pointer;
-			//typedef typename ft::iterator<T> 							iterator;
-			typedef size_t												size_type;
+			// + + + + + + + + + Member type
+			typedef T										value_type;
+			typedef Alloc									allocator_type;
+			typedef typename Allocator::reference			reference;
+			typedef typename Allocator::const_reference		const_reference;
+			typedef typename Allocator::pointer				pointer;
+			typedef typename Allocator::const_pointer		const_pointer;
+			typedef size_t									size_type;
+			//typedef typename ft::iterator<T> 				iterator;
 		private:
 			pointer array;
 			allocator_type _alloc;
 		public:
+			// + + + + + + + + + Member functions 
 			/* default */
+			explicit vector(const Allocator& = Allocator())
 			explicit vector (const allocator_type& alloc = allocator_type())
 			{
 				this->array = nullptr;
