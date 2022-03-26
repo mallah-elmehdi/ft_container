94c09c5866e199724c7b93620a10516a10ca90e1
mallah-elmehdi <elmehdimallah@gmail.com>
2022-03-26 01:28:18 +0100

    vect

--- vector/main.cpp
+++ vector/main.cpp
@@ -1,59 +1,26 @@
 #include <iostream>
 #include <vector>
-#include "vector.hpp"
-#include "iterator.hpp"
+// #include "vector.hpp"
+// #include "iterator.hpp"
 
 int main()
 {
-  std::cout << "====== vector ======\n";
-
-  ft::vector<int> v(100);
-  for (size_t i = 0; i < 100; i++) {
-    v[i] = i;
-  }
-  v.resize(10);
-  // std::cout << v.capacity() << "\n";
-  // for (std::vector<int>::iterator it = v.begin(); it != v.end(); it++) {
+  std::vector<int> v(5);
   try {
-    std::cout << v.at(9) << std::endl;
 
+    v.reserve(v.max_size() + 100);
   }
-  catch (const std::out_of_range& oor) {
-    std::cerr << "Out of Range error: " << oor.what() << '\n';
-  }
-  // }
+  catch(const std::length_error& le)
+  {
+    std::cerr << le.what() << '\n';
 
-  // std::cout << "====== ft ======\n";
-  // ft::vector<int> v(10);
-  // for (size_t i = 0; i < 10; i++) {
-  //   v[i] = i;
-  //   std::cout << v[i] << std::endl;
+}
+  // ft::vector<int>::iterator _it = v.begin();
+  // _it += 5;
+  // v.insert(_it, 1, 3);
+  // for (ft::vector<int>::iterator it = v.begin() ; it != v.end(); it++) {
+    // std::cout << *it << std::endl;
   // }
-  // for (ft::vector<int>::iterator it = v.begin(); it != v.end(); it++) {
-  //   std::cout << [] << std::endl;
-  // }
-
-  // ft::vector<int> iv(1);
-  // std::cout << iv.empty() << std::endl;
-  // std::cout << v.size() << std::endl;
-  // std::cout << v.capacity() << std::endl;
-  // v.resize(30, 9);
-  // std::cout << v.size() << std::endl;
-  // std::cout << v.capacity() << std::endl;
-  // for (size_t i = 0; i < 10; i++) {
-  //     std::cout << v[i] << std::endl;
-  // }
-	// int *ptr1 = new int[10];
-	// int *ptr2 = new int[10];
-	//
-	// for (size_t i = 0; i < 10; i++) {
-	// 	ptr1[i] = 12;
-	// 	ptr2[i] = 22;
-	// }
-	//
-	// ft::iterator<int> it;
-	// ft::iterator<int> it1(ptr1);
-	// ft::iterator<int> it2(it1);
-	//
-	// std::cout << *it2.base() << std::endl;
+  std::cout  << "capacity = " << v.capacity() << "\n";
+  std::cout  << "size = " << v.size() << "\n";
 }
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -45,19 +45,47 @@ namespace ft {
 					this->vect[i] = val;
 				}
 			}
-			// //range
-			// template <class InputIterator> vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type())
+			//range
+			// template <class InputIterator>
+			// vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()) : allocator(alloc)
 			// {
 			//
 			// }
-			// //copy
-			// vector (const vector& x)
-			// {
-			//
-			// }
+			//copy
+			vector (const vector& x)
+			{
+				this->_size = x._size;
+				this->_capacity = x._capacity;
+				this->allocator = x.allocator;
+				if (this->_capacity)
+				{
+					this->vect = this->allocator.allocate(this->_capacity);
+					for (size_type i = 0; i < this->_size; i++) {
+						this->vect[i] = x.vect[i];
+					}
+				}
+			}
+			vector& operator=(const vector& x)
+			{
+				this->~vector();
+				this->_size = x._size;
+				this->_capacity = x._size;
+				this->allocator = x.allocator;
+				if (this->_capacity)
+				{
+					this->vect = this->allocator.allocate(this->_capacity);
+					for (size_type i = 0; i < this->_size; i++) {
+						this->vect[i] = x.vect[i];
+					}
+				}
+				return (*this);
+			}
 			/* Destructor */
 			~vector()
 			{
+				for (size_t i = 0; i < this->_size; i++) {
+					this->pop_back();
+				}
 				this->allocator.deallocate(this->vect, this->_capacity);
 			};
 			/* Iterators */
@@ -144,6 +172,8 @@ namespace ft {
 			}
 			void reserve(size_type n)
 			{
+				if (n > this->max_size())
+					throw std::length_error("length_error");
 				if (this->_capacity < n)
 				{
 					pointer temp_vect = this->allocator.allocate(n);
@@ -176,6 +206,65 @@ namespace ft {
 					return (this->vect[n]);
 				throw std::out_of_range("out_of_range");
 			}
+			reference front()
+			{
+				return (*this->begin());
+			}
+			const_reference front() const
+			{
+				return (*this->begin());
+			}
+			reference back()
+			{
+				return (*(this->end() - 1));
+			}
+			const_reference back() const
+			{
+				return (*(this->end() - 1));
+			}
+			/* Modifiers */
+			// range
+			// template <class InputIterator> void assign (InputIterator first, InputIterator last);
+			// fill
+			// void assign (size_type n, const value_type& val);
+			void push_back (const value_type& val)
+			{
+				if (this->_size == this->_capacity)
+				{
+					size_type temp_capacity = this->_capacity;
+					this->_capacity = this->_capacity ? this->_capacity * 2 : 1;
+					pointer temp_vect = this->allocator.allocate(this->_capacity);
+					for (size_type i = 0; i < this->_size; i++) {
+						temp_vect[i] = this->vect[i];
+					}
+					//dealocate the vect
+					this->allocator.deallocate(this->vect, temp_capacity);
+					temp_vect[this->_size] = val;
+					//change size
+					this->vect = temp_vect;
+				}
+				else
+					this->vect[this->_size] = val;
+				this->_size++;
+			}
+			void pop_back()
+			{
+				this->allocator.destroy(this->back());
+				this->_size--;
+			}
+			// single element
+			iterator insert(iterator position, const value_type& val)
+			{
+
+			}
+			// fill
+			void insert(iterator position, size_type n, const value_type& val)
+			{
+
+			}
+			// range
+			// template <class InputIterator>
+			// void insert (iterator position, InputIterator first, InputIterator last);
 			/* Allocator */
 			allocator_type get_allocator(void) const
 			{
