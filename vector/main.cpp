31e37a0efdedd11bc1186d0c0573c33d83a59791
mallah-elmehdi <elmehdimallah@gmail.com>
2022-03-30 14:22:43 +0000

    insert

--- vector/main.cpp
+++ vector/main.cpp
@@ -29,79 +29,73 @@
 // BFS algorithm in C++
 
 #include <iostream>
-#include <list>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-class Graph {
-  int numVertices;
-  list<int>* adjLists;
-  bool* visited;
+class Bobnet {
+    private:
+        int nodes;
+        int links;
+        int gatways;
+        vector<int> *graph;
+        vector<int> gates;
+    public:
+        Bobnet()
+        {
+            cin >> nodes >> links >> gatways; cin.ignore();
+            graph = new vector<int>[nodes];
+            for (int i = 0; i < links; i++) {
+                int n1, n2;
+                cin >> n1 >> n2; cin.ignore();
+                if (find(graph[n1].begin(), graph[n1].end(), n2) == graph[n1].end())
+                    graph[n1].push_back(n2);
+                if (find(graph[n2].begin(), graph[n2].end(), n1) == graph[n2].end())
+                    graph[n2].push_back(n1);
+            }
+            for (size_t i = 0; i < nodes; i++) {
+              for (std::vector<int>::iterator it = graph[i].begin(); it != graph[i].end(); it++) {
+                cout << *it;
+              }
+            }
+            for (int i = 0; i < gatways; i++) {
+                int ei;
+                cin >> ei; cin.ignore();
+                gates.push_back(ei);
+              }
+        }
+        void sever_link(int start_node)
+        {
+            vector<bool> visted(nodes, false);
+            vector<int> q;
 
-   public:
-  Graph(int vertices);
-  void addEdge(int src, int dest);
-  void BFS(int startVertex);
+            q.push_back(start_node);
+            while (!q.empty())
+            {
+                visted[q.front()] = true;
+                for (
+                    vector<int>::iterator it = graph[q.front()].begin();
+                    it != graph[q.front()].end();
+                    it++
+                )
+                {
+                    if (find(gates.begin(), gates.end(), *it) != gates.end())
+                        cout << q.front() << " " << *it << endl;
+                    q.push_back(*it);
+                }
+                q.erase(q.begin());
+            }
+        }
 };
 
-// Create a graph with given vertices,
-// and maintain an adjacency list
-Graph::Graph(int vertices) {
-  numVertices = vertices;
-  adjLists = new list<int>[vertices];
-}
-
-// Add edges to the graph
-void Graph::addEdge(int src, int dest) {
-  adjLists[src].push_back(dest);
-  adjLists[dest].push_back(src);
-  for (int i = 0; i < 4; i++) {
-    for (list<int>::iterator it = adjLists[i].begin(); it != adjLists[i].end(); it++) {
-      cout << *it  << " ";
+int main()
+{
+    Bobnet bob;
+    while (1) {
+        int si;
+        cin >> si; cin.ignore();
+        bob.sever_link(si);
     }
-    cout << "\n**\n";
-  }
-  cout << "\n------------------\n";
-}
-
-// BFS algorithm
-void Graph::BFS(int startVertex) {
-  visited = new bool[numVertices];
-  for (int i = 0; i < numVertices; i++)
-    visited[i] = false;
-
-  list<int> queue;
-
-  visited[startVertex] = true;
-  queue.push_back(startVertex);
-
-  list<int>::iterator i;
-
-  while (!queue.empty()) {
-    int currVertex = queue.front();
-    cout << "Visited " << currVertex << " ";
-    queue.pop_front();
-
-    for (i = adjLists[currVertex].begin(); i != adjLists[currVertex].end(); ++i) {
-      int adjVertex = *i;
-      if (!visited[adjVertex]) {
-        visited[adjVertex] = true;
-        queue.push_back(adjVertex);
-      }
-    }
-  }
-}
-
-int main() {
-  Graph g(4);
-  g.addEdge(0, 1);
-  g.addEdge(0, 2);
-  g.addEdge(1, 2);
-  g.addEdge(2, 0);
-  g.addEdge(2, 3);
-  g.addEdge(3, 3);
-
-  g.BFS(2);
-
-  return 0;
 }
