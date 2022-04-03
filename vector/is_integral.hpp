58e0e6847a9ff86b54a4c6274f3dcddbea5bcb44
mallah-elmehdi <elmehdimallah@gmail.com>
2022-04-03 02:27:27 +0000

    vector almost done

--- vector/is_integral.hpp
+++ vector/is_integral.hpp
@@ -3,41 +3,37 @@
 // * LIBRARIES *
 #include "integral_constant.hpp"
 #include <cstdint>
-// * [] *
 //is_integral class template
-namespace ft
-{
-    template <class T>
-    class is_integral : public integral_constant<bool, false> {};
-    template <>
-    class is_integral<bool> : public integral_constant<bool, true> {};
-    template <>
-    class is_integral<char> : public integral_constant<bool, true> {};
-    template <>
-    class is_integral<char16_t> : public integral_constant<bool, true> {};
-    template <>
-    class is_integral<char32_t> : public integral_constant<bool, true> {};
-    template <>
-    class is_integral<wchar_t> : public integral_constant<bool, true> {};
-    template <>
-    class is_integral<signed char> : public integral_constant<bool, true> {};
-    template <>
-    class is_integral<short int> : public integral_constant<bool, true> {};
-    template <>
-    class is_integral<int> : public integral_constant<bool, true> {};
-    template <>
-    class is_integral<long int> : public integral_constant<bool, true> {};
-    template <>
-    class is_integral<long long int> : public integral_constant<bool, true> {};
-    template <>
-    class is_integral<unsigned char> : public integral_constant<bool, true> {};
-    template <>
-    class is_integral<unsigned short int> : public integral_constant<bool, true> {};
-    template <>
-    class is_integral<unsigned int> : public integral_constant<bool, true> {};
-    template <>
-    class is_integral<unsigned long int> : public integral_constant<bool, true> {};
-    template <>
-    class is_integral<unsigned long long int> : public integral_constant<bool, true> {};
-}
+template <class T>
+class is_integral : public integral_constant<bool, false> {};
+template <>
+class is_integral<bool> : public integral_constant<bool, true> {};
+template <>
+class is_integral<char> : public integral_constant<bool, true> {};
+template <>
+class is_integral<char16_t> : public integral_constant<bool, true> {};
+template <>
+class is_integral<char32_t> : public integral_constant<bool, true> {};
+template <>
+class is_integral<wchar_t> : public integral_constant<bool, true> {};
+template <>
+class is_integral<signed char> : public integral_constant<bool, true> {};
+template <>
+class is_integral<short int> : public integral_constant<bool, true> {};
+template <>
+class is_integral<int> : public integral_constant<bool, true> {};
+template <>
+class is_integral<long int> : public integral_constant<bool, true> {};
+template <>
+class is_integral<long long int> : public integral_constant<bool, true> {};
+template <>
+class is_integral<unsigned char> : public integral_constant<bool, true> {};
+template <>
+class is_integral<unsigned short int> : public integral_constant<bool, true> {};
+template <>
+class is_integral<unsigned int> : public integral_constant<bool, true> {};
+template <>
+class is_integral<unsigned long int> : public integral_constant<bool, true> {};
+template <>
+class is_integral<unsigned long long int> : public integral_constant<bool, true> {};
 #endif
