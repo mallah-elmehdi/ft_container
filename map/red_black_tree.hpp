d555eb4d7bfa07d1a25720017021e875211a2f12
mallah-elmehdi <elmehdimallah@gmail.com>
2022-04-11 07:56:14 +0000

    map

--- map/red_black_tree.hpp
+++ map/red_black_tree.hpp
@@ -1,25 +1,46 @@
 #ifndef _RED_BLACK_TREE_
 #define _RED_BLACK_TREE_
 
+// aliases
+# define BLACK 1
+# define RED 0
+
 // create node
 template <class T>
 class Node {
 public:
+    // attribute
     T       data;
-    Node    *parent;
-    Node    *left;
-    Node    *right;
+    Node    *parent = NULL;
+    Node    *left = NULL;
+    Node    *right = NULL;
     int     color;
 }
 
 template <class T>
 class Red_Black_Tree {
 private:
-    Node *root;
-public:
-    Red_Black_Tree()
+    // attribute
+    Node *root = NULL;
+    // private member funtion
+    Node *initNode(T key)
     {
-
+        Node *newNode = new Node<T>();
+        newNode->data = key;
+        newNode->color = RED;
+        return (newNode);
+    }
+public:
+    Red_Black_Tree() {}
+    // inserting new node to the Tree
+    void insert(T key)
+    {
+        Node *newNode = initNode(key);
+        if (root == NULL)
+        {
+            root = newNode;
+            root->color = BLACK;
+        }
     }
 }
 
