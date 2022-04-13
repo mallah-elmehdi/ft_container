ba09b98b14e6b6ecd3cb70db777dc7e5f7c15df1
mallah-elmehdi <elmehdimallah@gmail.com>
2022-04-13 07:10:07 +0000

    map

--- map/map.hpp
+++ map/map.hpp
@@ -24,9 +24,10 @@ namespace ft
 			typedef typename allocator_type::const_reference		const_reference;
 			typedef typename allocator_type::pointer				pointer;
 			typedef typename allocator_type::const_pointer			const_pointer;
-			typedef ft::bd_iterator<value_type>						iterator;
             typedef ptrdiff_t										difference_type;
             typedef size_t											size_type;
+			typedef ft::bd_iterator<value_type>						iterator;
+			typedef ft::bd_iterator<const value_type>						const_iterator;
 
         private:
             Red_Black_Tree<key_type, mapped_type, Compare, allocator_type>	tree;
@@ -45,8 +46,15 @@ namespace ft
             //        const allocator_type& alloc = allocator_type());
             //
             // map (const map& x);
-			// 
-			iterator begin() { return (iterator(tree.begin())); }
+			
+			/* Iterators */
+			iterator begin()
+			{ 
+				return (iterator(tree.begin()));
+			}
+			const_iterator begin() const { return (const_iterator(tree.begin())); }
+			
+			
 			// Capacity
 			bool empty() const { return (size() == 0); }
 			size_type size() const { return (_size); }
