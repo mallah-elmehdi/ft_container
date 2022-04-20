03650fdc0617d64ff34a7c96812a3f9464a58c22
mallah-elmehdi <elmehdimallah@gmail.com>
2022-04-20 05:04:58 +0000

    map [DONE] - FINALE TOUCHES

--- map/main.cpp
+++ map/main.cpp
@@ -1,11 +1,48 @@
 #include "ft.hpp"
 #include <iostream>
+#include <iomanip>
 #include <map>
 
 #define LENGTH 100
+// Reset
+#define Color_Off '\033[0m'       // Text Reset
+// Bold
+#define BBlack '\033[1;30m'       // Black
+#define BRed '\033[1;31m'         // Red
+#define BGreen '\033[1;32m'       // Green
+#define BYellow '\033[1;33m'      // Yellow
+#define BBlue '\033[1;34m'        // Blue
+#define BPurple '\033[1;35m'      // Purple
+#define BCyan '\033[1;36m'        // Cyan
+#define BWhite '\033[1;37m'       // White
+// Background
+#define On_Black '\033[40m'       // Black
+#define On_Red '\033[41m'         // Red
+#define On_Green '\033[42m'       // Green
+#define On_Yellow '\033[43m'      // Yellow
+#define On_Blue '\033[44m'        // Blue
+#define On_Purple '\033[45m'      // Purple
+#define On_Cyan '\033[46m'        // Cyan
+#define On_White '\033[47m'       // White
+
 
 std::map<int, int> std_map;
 ft::map<int, int> ft_map;
+
+void ok()
+{
+	std::cout << " =========== [OK]\n";
+}
+
+void ko()
+{
+	std::cout << " =========== [KO]\n";
+}
+
+void title(std::string str)
+{
+	std::cout << "+ " << std::setw(20) << std::left << str;
+}
 
 void init_map()
 {
@@ -18,15 +55,15 @@ void init_map()
 
 void check_values(std::map<int, int> _std, ft::map<int, int> _ft)
 {
-	for (int i = 0; i < LENGTH; i++)
+	for (int i = 1; i <= LENGTH; i++)
 	{
-		if ((int)_std[i] != (int)_ft[i]);
+		if (_std[i] != _ft[i])
 		{
-			std::cout << " =========== [KO]\n";
+			ko();
 			return ;
 		}
 	}
-	std::cout << " =========== [OK]\n";
+	ok();
 }
 
 void member_functions()
@@ -34,15 +71,15 @@ void member_functions()
 	//
 	std::cout << "\n-------------------- Member functions\n\n";
 	
-	std::cout << "+ copy constructor";
+	title("copy constructor");
 	std::map<int, int> std_map_copy(std_map);
 	ft::map<int, int> ft_map_copy(ft_map);
 	check_values(std_map_copy, ft_map_copy);
 	
-	std::cout << "+ range constructor";
+	title("range constructor");
 	std::map<int, int> std_map_range(std_map.begin(), std_map.end());
 	ft::map<int, int> ft_map_range(ft_map.begin(), ft_map.end());
-	check_values(std_map_copy, ft_map_copy);
+	check_values(std_map_range, ft_map_range);
 }
 
 int main()
@@ -51,4 +88,5 @@ int main()
 	init_map();
 	// 
 	member_functions();
+
 }
---
+++ push.sh
@@ -0,0 +1,54 @@
+
+
+#!/bin/bash
+# Reset
+Color_Off='\033[0m'       # Text Reset
+# Bold
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+# Background
+On_Black='\033[40m'       # Black
+On_Red='\033[41m'         # Red
+On_Green='\033[42m'       # Green
+On_Yellow='\033[43m'      # Yellow
+On_Blue='\033[44m'        # Blue
+On_Purple='\033[45m'      # Purple
+On_Cyan='\033[46m'        # Cyan
+On_White='\033[47m'       # White
+
+push_num=1
+
+if [ -z "$1" ]
+    then
+        echo -e "${BRed}${On_White}Please specify the main directory path${Color_Off}"
+    else
+        if [ -z "$2" ]
+            then
+                echo -e "${BRed}${On_White}Please specify a commit message${Color_Off}"
+            else
+                cd $1
+                while true
+                do
+                        clear
+                        echo -e "${BBlue}${On_White}Push number $push_num : Waiting...${Color_Off}"
+                        sleep 1200
+                        echo
+                        echo -e "${BGreen}${On_White}Push changes starting...${Color_Off}"
+                        git add .
+                        #echo -en "${BPurple}${On_White}please enter the commit message: ${Color_Off}"
+                        #read commitmessage
+                        #git commit -m "$commitmessage"
+                        echo -en "${BPurple}${On_White}Commit message is: ${Color_Off}${BBlue}${On_White}$2 ${Color_Off}"
+                        sleep 2
+                        git commit -m "$2"
+                        git push
+                        ((push_num=push_num+1))
+                done
+        fi
+fi
