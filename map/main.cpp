c089c15bd8ed3e0bcd82fc70d4ae03bc1145b344
mallah-elmehdi <elmehdimallah@gmail.com>
2022-04-20 04:44:54 +0000

    map [DONE] - FINALE TOUCHES

--- map/main.cpp
+++ map/main.cpp
@@ -2,36 +2,47 @@
 #include <iostream>
 #include <map>
 
+#define LENGTH 100
 
-ft::map<int, int> ft_map;
 std::map<int, int> std_map;
+ft::map<int, int> ft_map;
 
 void init_map()
 {
-	for (size_t i = 1; i <= 100; i++)
+	for (int i = 1; i <= LENGTH; i++)
 	{
-		ft_map.insert(ft::make_pair(i, i*10));
 		std_map.insert(std::make_pair(i, i*10));
+		ft_map.insert(ft::make_pair(i, i*10));
 	}
+}
+
+void check_values(std::map<int, int> _std, ft::map<int, int> _ft)
+{
+	for (int i = 0; i < LENGTH; i++)
+	{
+		if ((int)_std[i] != (int)_ft[i]);
+		{
+			std::cout << " =========== [KO]\n";
+			return ;
+		}
+	}
+	std::cout << " =========== [OK]\n";
 }
 
 void member_functions()
 {
 	//
-	std::cout << "-------------------- Member functions\n";
-	std::cout << "+ copy constructor\n";
-	ft::map<int, int> ft_map_copy(ft_map);
-	std::map<int, int> std_map_copy(std_map);
-	std::cout << "+ range constructor\n";
-	ft::map<int, int> ft_map_range(ft_map.begin(), ft_map.end());
-	ft::map<int, int> std_map_range(std_map.begin(), std_map.end());
-
-
+	std::cout << "\n-------------------- Member functions\n\n";
 	
-
-	std::cout << "+ [ STD ] + (constructor)\n";
-
-	std::map<int, int> std_map;
+	std::cout << "+ copy constructor";
+	std::map<int, int> std_map_copy(std_map);
+	ft::map<int, int> ft_map_copy(ft_map);
+	check_values(std_map_copy, ft_map_copy);
+	
+	std::cout << "+ range constructor";
+	std::map<int, int> std_map_range(std_map.begin(), std_map.end());
+	ft::map<int, int> ft_map_range(ft_map.begin(), ft_map.end());
+	check_values(std_map_copy, ft_map_copy);
 }
 
 int main()
@@ -39,4 +50,5 @@ int main()
 	//
 	init_map();
 	// 
+	member_functions();
 }
